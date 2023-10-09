71892549d19579927d320601d041c97ab904fc40
ETorresSacha <eriktorressacha@gmail.com>
2023-10-08 21:43:49 -0500

    project

--- src/components/landingPage/opcionesNav/Projects.jsx
+++ src/components/landingPage/opcionesNav/Projects.jsx
@@ -54,48 +54,51 @@ const Projects = () => {
       }}
     >
       <h1 className="h4-landing">MIS PROYECTOS</h1>
-      <div class="card mb-3" style={{ maxWidth: "540px", margin: "15px" }}>
+      <div
+        class="card mb-3"
+        className="conteiner-card"
+        style={{ maxWidth: "540px", margin: "15px" }}
+      >
         <div class="row g-0">
           <div class="col-md-4">
             <img
               src="../../../../image/clothes.jpg"
-              class="img-fluid rounded-start"
               style={{
                 width: "100%",
                 height: "100%",
+                borderRadius: "10px 0px 0px 10px",
               }}
               alt="production"
             />
           </div>
-          <div class="col-md-8">
+          <div
+            class="col-md-8"
+            style={{ padding: "10px", border: "solid transparent" }}
+          >
             <div class="card-body">
-              <h5 class="card-title">ONLINE STORE</h5>
+              <h5
+                class="card-title"
+                style={{ color: "white", margin: "2px 0 10px auto" }}
+              >
+                ONLINE STORE
+              </h5>
               <p class="card-text">
                 Esta página es un ecommers, en esta tienda virtual puedes
                 encontrar las mejores prendas que estan de moda y a los mejores
                 precios.
               </p>
-              <div
-                class="card-text"
-                style={{
-                  display: "flex",
-                  flexWrap: "wrap",
-                  gap: "10px",
-                  alignContent: "center",
-                  alignItems: "center",
-                  justifyContent: "center",
-                }}
-              >
+              <div class="card-text" className="icon-link-project">
                 <a
                   href="https://github.com/ETorresSacha/STORE-ONLINE-FRONT"
                   target="_blank"
                   rel="noreferrer"
+                  className="icon"
                 >
                   <GitHubIcon
                     style={{
                       color: "black",
-                      border: "solid 1px",
-                      borderRadius: "50%",
+                      borderRadius: "50% ",
+                      background: "white",
                     }}
                   />
                 </a>
@@ -127,57 +130,48 @@ const Projects = () => {
           display: "flex",
           flexWrap: "wrap",
           padding: "15px",
-          //margin: "30px",
-          //display: "grid",
-          //gridTemplateColumns: "repeat(2,minmax(350px,1fr))",
           justifyContent: "space-evenly",
-          //placeContent: "center",
         }}
       >
         {proyectos.map((element) => {
           return (
-            <div
-              key={element.id}
-              class="card mb-3"
-              style={{ maxWidth: "540px" }}
-            >
+            <div key={element.id} class="card mb-3" className="conteiner-card">
               <div class="row g-0">
                 <div class="col-md-4">
                   <img
                     src={element.video}
-                    class="img-fluid rounded-start"
                     style={{
                       width: "100%",
                       height: "100%",
+                      borderRadius: "10px 0px 0px 10px",
                     }}
                     alt="..."
                   />
                 </div>
-                <div class="col-md-8">
+                <div
+                  class="col-md-8"
+                  style={{ padding: "10px", border: "solid transparent" }}
+                >
                   <div class="card-body">
-                    <h5 class="card-title">{element.title}</h5>
-                    <p class="card-text">{element.description}</p>
-                    <p
-                      class="card-text"
-                      style={{
-                        display: "flex",
-                        flexWrap: "wrap",
-                        gap: "10px",
-                        alignContent: "center",
-                        alignItems: "center",
-                        justifyContent: "center",
-                      }}
+                    <h5
+                      class="card-title"
+                      style={{ color: "white", margin: "2px 0 10px auto" }}
                     >
+                      {element.title}
+                    </h5>
+                    <p class="card-text">{element.description}</p>
+                    <p class="card-text" className="icon-link-project">
                       <a
                         href={element.links.gitHub}
                         target="_blank"
                         rel="noreferrer"
+                        className="icon"
                       >
                         <GitHubIcon
                           style={{
                             color: "black",
-                            border: "solid 1px",
-                            borderRadius: "50%",
+                            borderRadius: "50% ",
+                            background: "white",
                           }}
                         />
                       </a>
@@ -186,13 +180,23 @@ const Projects = () => {
                         href={element.links.youtube}
                         target="_blank"
                         rel="noreferrer"
+                        className="icon"
                       >
-                        <YouTubeIcon style={{ color: "red" }} />
+                        <YouTubeIcon
+                          style={{
+                            color: "red",
+                            width: "auto",
+                            background: "white",
+                            borderRadius: "50% ",
+                            margin: "5px",
+                          }}
+                        />
                       </a>
                       <a
                         href={element.links.depoly}
                         target="_blank"
                         rel="noreferrer"
+                        className="icon"
                       >
                         <button type="button" class="btn btn-outline-primary">
                           Ver la página
