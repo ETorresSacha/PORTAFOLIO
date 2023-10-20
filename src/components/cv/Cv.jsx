6057d72f8f1893a2db8559d0bd3b78eab69add07
ETorresSacha <eriktorressacha@gmail.com>
2023-10-20 10:55:59 -0500

    descargar

--- src/components/cv/Cv.jsx
+++ src/components/cv/Cv.jsx
@@ -1,15 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 import erik from "./erik.pdf";
 
 const Cv = () => {
+  useEffect(() => {
+    if (typeof window.orientation !== "undefined") {
+      document.getElementById("enlaceDescargarPdf").click();
+      window.close();
+    }
+  }, []);
   return (
     <div style={{ position: "absolute", width: "100%", height: "100%" }}>
-      <object
-        data={erik}
-        type="application/pdf"
-        width="100%"
-        height="100%"
-      ></object>
+      <object data={erik} type="application/pdf" width="100%" height="100%">
+        <br />
+        <a href={erik} id="enlaceDescargarPdf" download={erik}></a>
+      </object>
     </div>
   );
 };
