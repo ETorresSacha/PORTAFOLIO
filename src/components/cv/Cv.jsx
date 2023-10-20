8cefc764e82519e0673a01b160d60b3ed8dcbfce
ETorresSacha <eriktorressacha@gmail.com>
2023-10-20 11:18:03 -0500

    descargar

--- src/components/cv/Cv.jsx
+++ src/components/cv/Cv.jsx
@@ -2,17 +2,24 @@ import React, { useEffect } from "react";
 import erik from "./erik.pdf";
 
 const Cv = () => {
+  const descargar = () => {
+    const link = document.createElement("a");
+    link.href = resume;
+    link.download = "MagaliPereyra.pdf";
+    link.click();
+  };
   useEffect(() => {
     if (typeof window.orientation !== "undefined") {
       document.getElementById("enlaceDescargarPdf").click();
       window.close();
     }
-  }, []);
+  }, [window.orientation]);
+
   return (
     <div style={{ position: "absolute", width: "100%", height: "100%" }}>
       <object data={erik} type="application/pdf" width="100%" height="100%">
         <br />
-        <a href={erik} id="enlaceDescargarPdf" download={erik}></a>
+        <a href={erik} id="enlaceDescargarPdf"></a>
       </object>
     </div>
   );
