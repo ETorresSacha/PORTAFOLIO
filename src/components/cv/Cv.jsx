3b5a000797724c777154e23d3781872cfad911a2
ETorresSacha <eriktorressacha@gmail.com>
2023-10-20 13:45:05 -0500

    icono

--- src/components/cv/Cv.jsx
+++ src/components/cv/Cv.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import erik from "./erik.pdf";
 
 const Cv = () => {
@@ -10,14 +10,9 @@ const Cv = () => {
   }, [window.orientation]);
 
   return (
-    <div style={{ position: "absolute", width: "100%", height: "100%" }}>
-      <object data={erik} type="application/pdf" width="100%" height="100%">
-        <a
-          href={erik}
-          id="enlaceDescargarPdf"
-          download="ErikTorres.pdf"
-          onClick={descargar}
-        ></a>
+    <div className="absolute w-full h-full">
+      <object data={erik} type="application/pdf" className="w-full h-full">
+        <a href={erik} id="enlaceDescargarPdf" download="ErikTorres.pdf"></a>
       </object>
     </div>
   );
